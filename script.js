3b9bda814558ffa50c4e050681104595c4987c89
mattheushk <mattheus.krainski@escola.pr.gov.br>
2025-11-14 00:51:32 -0300

    UPDATE

--- script.js
+++ script.js
@@ -8,17 +8,6 @@ document.querySelectorAll('.icone').forEach(icon => {
   });
 });
 
-// ===== 2. Saudação dinâmica =====
-window.addEventListener('load', () => {
-  const hora = new Date().getHours();
-  let saudacao = '';
-
-  if (hora < 12) saudacao = 'Bom dia!';
-  else if (hora < 18) saudacao = 'Boa tarde!';
-  else saudacao = 'Boa noite!';
-
-  alert(`${saudacao} Seja bem-vindo(a) à minha página!`);
-});
 
 // ===== 3. Clique nas imagens com efeito =====
 const atividades = document.querySelectorAll('.atividade');
