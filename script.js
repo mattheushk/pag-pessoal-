7b141c98cff6a8f1da704ddff42c6188c6359739
mattheushk <mattheus.krainski@escola.pr.gov.br>
2025-11-14 22:01:22 -0300

    update

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-// ===== 1. Tema claro/escuro =====
-const botaoTema = document.getElementById("tema-btn");
-let temaClaro = false;
-
-botaoTema.addEventListener("click", () => {
-  temaClaro = !temaClaro;
-
-  document.body.classList.toggle("light");
-
-  botaoTema.textContent = temaClaro 
-    ? "🌙 Modo Escuro" 
-    : "☀️ Modo Claro";
-});
-
-// ===== 2. Efeito nas imagens =====
-document.querySelectorAll('.atividade').forEach(a => {
-  a.addEventListener('mouseover', () => {
-    a.style.transform = 'scale(1.05)';
+// ===== Efeito nas imagens =====
+document.querySelectorAll('.atividade').forEach(item => {
+  item.addEventListener('mouseover', () => {
+    item.style.transform = 'scale(1.05)';
   });
-  a.addEventListener('mouseout', () => {
-    a.style.transform = 'scale(1)';
+
+  item.addEventListener('mouseout', () => {
+    item.style.transform = 'scale(1)';
   });
 });
