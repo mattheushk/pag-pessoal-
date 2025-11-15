3bbb84fcba7ebcd2f3e576bc3faa18883f68b231
mattheushk <mattheus.krainski@escola.pr.gov.br>
2025-11-14 23:06:28 -0300

    update

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ document.querySelectorAll('.atividade').forEach(item => {
     item.style.transform = 'scale(1)';
   });
 });
+document.querySelectorAll('.atividade').forEach(item => {
+  item.addEventListener('mouseover', () => {
+    item.style.transform = 'scale(1.05)';
+  });
+
+  item.addEventListener('mouseout', () => {
+    item.style.transform = 'scale(1)';
+  });
+});
